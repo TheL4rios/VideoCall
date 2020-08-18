49284292ef0acf66a2874cb90326e8dc78cd8311
Arturo Larios <arallariosqu@ittepic.edu.mx>
2020-08-18 00:31:31 -0600

    added visualitation of the camera

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require('express');
-
+var bodyParser = require('body-parser');
 const app = new express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 8080;
 
-app.listen(3000, () => {
-    console.log('Running...');
+http.listen(port, () => {
+    console.log('Running in port ' + port);
 });
 
-app.use(express.static('page'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
 
-/*app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/page/' + 'index.html');
+/*io.on('connection', (socket) => {
+    socket.on('stream', (image) => {
+        socket.broadcast.emit('stram', image);
+    });
+});*/
+
+app.post('/room.html', function(request, response) {
+    if (request.body.action == 'create') {
+        const room = request.body.room;
+        const pass = request.body.password;
+        
+        io.on('connection', (socket) => {
+            response.status(200);
+            socket.join(room + pass);
+            response.redirect('/room.html');
+            io.to(room + pass).emit('messages', 'hi');
+        });
+    } else if (request.body.action == 'join') {
+        
+    } else {
+        
+    }
+});
+
+/*io.on('connection', (socket) => {
+    socket.on('stream', (image) => {
+        socket.broadcast.emit('stream', image);
+    });
 });*/
---
+++ public/js/room.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', function() {
+    init();
+});
+
+navigator.getUserMedia = (navigator.getUserMedia        ||
+                          navigator.webkitGetUserMedia  ||
+                          navigator.mozGetUserMedia     ||
+                          navigator.msgGetUserMedia);
+let mic = true;
+let camera = true;
+const socket = io();
+
+socket.on('messages', (data) => {
+    console.log(data);
+    document.getElementById('messages').append(data);
+});
+
+init = function() {
+    
+    initCamera(camera, mic);
+}
+
+function initCamera(video, mic) {
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: video, audio: mic},loadCamera,() => {});
+    }
+
+    videoAdjust();
+}
+
+function loadCamera(stream){
+    const video = document.getElementById('video');
+
+    try {
+        video.srcObject = stream;
+    } catch (error) {
+        video.src = URL.createObjectURL(stream);
+    }
+}
+
+function videoAdjust() {
+    let videos = document.getElementsByTagName('video');
+    let percent = 0;
+
+    if (videos.length > 4) {
+        percent = (100 / 4) - 1;
+    } else {
+        percent = (100 / videos.length) - 1;
+    }
+
+    for (let i = 0; i < videos.length; i++) {
+        videos[i].style.width = percent + '%';
+        videos[i].style.marginLeft = '0.5%';
+    }
+}
+
+document.getElementById('mic').addEventListener('click', () => {
+    mic = !mic;
+    
+    if (!mic) {
+        document.getElementById('mic-img').src = '../img/icons/mic-off.png';
+    } else {
+        document.getElementById('mic-img').src = '../img/icons/mic.png';
+    }
+
+    initCamera(camera, mic);
+});
+
+document.getElementById('camera').addEventListener('click', () => {
+    camera = !camera;
+    
+    if (!camera) {
+        document.getElementById('camera-img').src = '../img/icons/camera-off.png';
+    } else {
+        document.getElementById('camera-img').src = '../img/icons/camera.png';
+    }
+
+    initCamera(camera, mic);
+});
+
+document.getElementById('call').addEventListener('click', () => {
+    
+});
