2fd2c479ea2390589e3ce9f4e5a7bbe6346c0829
Arturo Larios <arallariosqu@ittepic.edu.mx>
2020-08-20 01:45:42 -0600

    added video and messages modified

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+const fs = require('fs');
 const express = require('express');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = new express();
-const http = require("http").createServer(app);
+const http = require("https").createServer({
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.crt')
+}, app);
 const io = require("socket.io")(http);
 const port = process.env.PORT || 8080;
+const peer = require('peer');
 
-http.listen(port, () => {
-    console.log('Running in port ' + port);
-});
-
+app.use('/peerjs', peer.ExpressPeerServer(http, {
+    debug: true
+}));
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
@@ -24,16 +28,19 @@ app.get('/room.html/:data', (request, response) => {
 });
 
 io.on('connection', (socket) => {
-    socket.on('join', (room) => {
+    socket.on('join', (room, peerId) => {
         socket.join(room);
-
+        socket.to(room).broadcast.emit('user-connected', peerId);
         socket.on('message', (message) => {
-            io.to(room).emit('newMessage', message);
+            io.to(room).emit('new-message', message);
         });
 
-        socket.on('stream', (stream) => {
-            socket.broadcast.emit('newStream', stream);
-            //io.to(room).emit('newStream', stream);
+        socket.on('disconnect', () => {
+            socket.to(room).broadcast.emit('user-disconnected', peerId);
         });
     });
+});
+
+http.listen(port, () => {
+    console.log('Running in port ' + port);
 });
--- public/js/room.js
+++ public/js/room.js
@@ -1,20 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     init();
 });
 
-const ID = String(window.location).split('/')[4];
+const ROOM_ID = String(window.location).split('/')[4];
+const peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: location.hostname,
+    port: location.port || (location.protocol === 'https:' ? 443 : 80)
+});
 
 navigator.getUserMedia = (navigator.getUserMedia        ||
                           navigator.webkitGetUserMedia  ||
                           navigator.mozGetUserMedia     ||
-                          navigator.msgGetUserMedia);
-let mic = true;
-let camera = true;
+                          navigator.msgGetUserMedia     ||
+                          navigator.mediaDevices);
+let localCamera;
+let allVideos = {};
 const socket = io();
 
-socket.emit('join', ID);
+peer.on('open', (peerId) => {
+    socket.emit('join', ROOM_ID, peerId);
+});
 
-socket.on('newMessage', (message) => {
+socket.on('user-disconnected', (peerId) => {
+    if (allVideos[peerId]) {
+        allVideos[peerId].remove(); 
+        videoAdjust();
+    }
+});
+
+socket.on('new-message', (message) => {
     const div = document.createElement('div');
     div.className = 'message';
     const p = document.createElement('p');
@@ -23,26 +38,45 @@ socket.on('newMessage', (message) => {
     document.getElementById('messages').appendChild(div);
 });
 
-socket.on('newStream', (stream) => {
-    loadCamera(stream);
-    videoAdjust();
-});
+function initCamera(video, mic) {
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: video, audio: mic},(stream) => {
+            localCamera = stream;
+            const video = document.createElement('video');
+            loadCamera(video, stream);
+            peerConnection(stream);
+        },() => {});
+    }
+}
 
 init = function() {
     initCamera(camera, mic);
 }
 
-function initCamera(video, mic) {
-    if (navigator.getUserMedia) {
-        navigator.getUserMedia({video: video, audio: mic},(stream) => {
-            loadCamera(stream);
-            socket.emit('stream', stream);
-        },() => {});
-    }
+function peerConnection(stream) {
+    const video = document.createElement('video');
+    peer.on('call', (call) => {
+        call.answer(stream);
+        call.on('stream', (videoStream) => {
+            loadCamera(video, videoStream);
+        });
+    });
+
+    socket.on('user-connected', (peerId) => {
+        connectToUser(peerId, stream);
+    });
 }
 
-function loadCamera(stream){
+function connectToUser(peerId, stream) {
     const video = document.createElement('video');
+    allVideos[peerId] = video;
+    const call = peer.call(peerId, stream);
+    call.on('stream', (videoStream) => {
+        loadCamera(video, videoStream);
+    });
+}
+
+function loadCamera(video, stream){
     video.className = 'video';
     video.autoplay = 'true';
     document.getElementById('video-section').appendChild(video);
@@ -52,50 +86,55 @@ function loadCamera(stream){
     } catch (error) {
         video.src = (window.URL || window.webkitURL).createObjectURL(stream);
     }
+
+    videoAdjust();
 }
 
 function videoAdjust() {
     let videos = document.getElementsByTagName('video');
-    let percent = 0;
+    let width = 0;
+    let height = 100;
 
-    if (videos.length > 4) {
-        percent = (100 / 4) - 1;
+    if (videos.length > 8) {
+        height = (100 / Math.ceil(videos.length / 8)) - 1;
+        width = (100 / 8) - 1;
     } else {
-        percent = (100 / videos.length) - 1;
+        width = (100 / videos.length) - 1;
     }
 
     for (let i = 0; i < videos.length; i++) {
-        videos[i].style.width = percent + '%';
+        videos[i].style.height = height + '%';
+        videos[i].style.width = width + '%';
         videos[i].style.marginLeft = '0.5%';
     }
 }
 
 document.getElementById('mic').addEventListener('click', () => {
-    mic = !mic;
+    let mic = localCamera.getAudioTracks()[0].enabled;
     
     if (!mic) {
-        document.getElementById('mic-img').src = '../img/icons/mic-off.png';
-    } else {
+        localCamera.getAudioTracks()[0].enabled = true;
         document.getElementById('mic-img').src = '../img/icons/mic.png';
+    } else {
+        localCamera.getAudioTracks()[0].enabled = false;
+        document.getElementById('mic-img').src = '../img/icons/mic-off.png';
     }
-
-    initCamera(camera, mic);
 });
 
 document.getElementById('camera').addEventListener('click', () => {
-    camera = !camera;
+    let camera = localCamera.getVideoTracks()[0].enabled;
     
     if (!camera) {
-        document.getElementById('camera-img').src = '../img/icons/camera-off.png';
-    } else {
+        localCamera.getVideoTracks()[0].enabled = true;
         document.getElementById('camera-img').src = '../img/icons/camera.png';
+    } else {
+        localCamera.getVideoTracks()[0].enabled = false;
+        document.getElementById('camera-img').src = '../img/icons/camera-off.png';
     }
-
-    initCamera(camera, mic);
 });
 
 document.getElementById('call').addEventListener('click', () => {
-    
+    window.history.back();
 });
 
 document.getElementById('message').addEventListener('keyup', (event) => {
